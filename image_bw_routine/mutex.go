6ad1166a92cabf7cc923a19b4480defa50287e46
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-08 08:39:00 +0200

    tentative mutex

--- image_bw_routine/mutex.go
+++ image_bw_routine/mutex.go
@@ -8,6 +8,7 @@ import (
 	"image/jpeg"
 	"log"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -15,7 +16,8 @@ import (
 Cette fonction va nous permettre d'analyser un bout de l'image à l'intérieur de la zone donnée par l'utilisateur
 On indique également le fichier d'entrée et de sortie
 */
-func analyze(upleftx int, uplefty int, width int, height int, input image.Image, final chan image.Image) {
+func analyze(upleftx int, uplefty int, width int, height int, input image.Image, final *image.RGBA, wg *sync.WaitGroup, m *sync.Mutex) {
+	m.Lock()
 	output := image.NewRGBA(input.Bounds())
 
 	for x := upleftx; x < upleftx+width; x++ {
@@ -27,12 +29,17 @@ func analyze(upleftx int, uplefty int, width int, height int, input image.Image,
 			output.Set(x, y, pixel)
 		}
 	}
-	final <- output
+	draw.DrawMask(final, final.Bounds(), output, image.ZP, final.Bounds(), image.ZP, draw.Over)
+	m.Unlock()
+	wg.Done()
 }
 
 func main() {
+	var wg sync.WaitGroup
+	var m sync.Mutex
+
 	startTime := time.Now()
-	file, err := os.Open("koala.jpg")
+	file, err := os.Open("testFAT.JPG")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,26 +50,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	finalImg := make(chan image.Image, 4)
+	finalImg := image.NewRGBA(img.Bounds())
 	x := img.Bounds().Max.X / 2
-
-	go analyze(0, 0, img.Bounds().Dx()/2, img.Bounds().Dy()/2, img, finalImg)
-	go analyze(x, 0, img.Bounds().Dx()/2, img.Bounds().Dy()/2, img, finalImg)
-	go analyze(0, x, img.Bounds().Dx()/2, img.Bounds().Dy()/2, img, finalImg)
-	go analyze(x, x, img.Bounds().Dx()/2, img.Bounds().Dy()/2, img, finalImg)
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			wg.Add(1)
+			go analyze(x*i, x*j, img.Bounds().Dx()/2, img.Bounds().Dy()/2, img, finalImg, &wg, &m)
+		}
+	}
+	wg.Wait()
 
 	outFile, err := os.Create("changed.jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer outFile.Close()
-	empty := image.NewRGBA(img.Bounds())
-	capacity := cap(finalImg)
-	for i := 0; i < capacity; i++ {
-		outputIMG := <-finalImg
-		draw.DrawMask(empty, empty.Bounds(), outputIMG, image.ZP, empty.Bounds(), image.ZP, draw.Over)
-	}
-	jpeg.Encode(outFile, empty, nil)
+	jpeg.Encode(outFile, finalImg, nil)
 
 	totalTime := time.Since(startTime)
 	fmt.Println("Durée totale : " + totalTime.String())
