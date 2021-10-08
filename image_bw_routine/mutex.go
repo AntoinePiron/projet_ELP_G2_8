ee155de236988f3d2c94be007b1f8f3526da040b
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-08 10:09:18 +0200

    nettoyage code mutex

--- image_bw_routine/mutex.go
+++ image_bw_routine/mutex.go
@@ -15,7 +15,7 @@ import (
 Cette fonction va nous permettre d'analyser un bout de l'image à l'intérieur de la zone donnée par l'utilisateur
 On indique également le fichier d'entrée et de sortie
 */
-func analyze(upleftx int, uplefty int, width int, height int, input image.Image, final *image.RGBA, wg *sync.WaitGroup, m *sync.Mutex) {
+func analyze(upleftx int, uplefty int, width int, height int, input image.Image, final *image.RGBA, wg *sync.WaitGroup) {
 
 	for x := upleftx; x < upleftx+width; x++ {
 		for y := uplefty; y < uplefty+height; y++ {
@@ -31,7 +31,6 @@ func analyze(upleftx int, uplefty int, width int, height int, input image.Image,
 
 func main() {
 	var wg sync.WaitGroup
-	var m sync.Mutex
 	file, err := os.Open("testFAT.JPG")
 	if err != nil {
 		log.Fatal(err)
@@ -51,7 +50,7 @@ func main() {
 	for i := 0; i < nbDiv; i++ {
 		for j := 0; j < nbDiv; j++ {
 			wg.Add(1)
-			go analyze(x*i, y*j, img.Bounds().Dx()/nbDiv, img.Bounds().Dy()/nbDiv, img, finalImg, &wg, &m)
+			go analyze(x*i, y*j, img.Bounds().Dx()/nbDiv, img.Bounds().Dy()/nbDiv, img, finalImg, &wg)
 		}
 	}
 	wg.Wait()
