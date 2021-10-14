b6c821a2f030fd46612ee609464c213a0748bbd6
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-14 09:37:32 +0200

    debut structure client

--- TCP/client_part/client.go
+++ TCP/client_part/client.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/jpeg"
 	"net"
 	"os"
 	"time"
 )
 
+//On va se créer le go object qu'on va envoyer
+type ImgForConn struct {
+	id      int
+	content image.Image
+}
+
 func main() {
+	file, err := os.Open("koala.jpg")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	toSend := ImgForConn{
+		0,
+		img,
+	}
+	fmt.Println(toSend)
+
 	conn, errconn := net.Dial("tcp", "127.0.0.1:10000")
 	if errconn != nil {
 		os.Exit(1)
