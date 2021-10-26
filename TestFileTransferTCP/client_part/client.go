49e38687bac16977f8e067ee468776e39242564e
AntoinePiron <pro.piron.antoine@gmail.com>
2021-10-26 17:56:15 +0200

    transmission d'image fonctionnelle

--- TestFileTransferTCP/client_part/client.go
+++ TestFileTransferTCP/client_part/client.go
@@ -47,3 +47,47 @@ func main() {
 	}
 	fmt.Println("Received file completely!")
 }
+
+func sendFileToServer(conn net.Conn) {
+	//On oublie pas de defer la fermture de la connection pour qu'elle se ferme automatiquement à la fin de l'execution
+	defer conn.Close()
+	//On ouvre le fichier et si jamais une erreur se produit on arrete la fonction avec le mot cle return
+	file, err := os.Open("koala.jpg")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 10)
+	fileName := fillString(fileInfo.Name(), 64)
+	fmt.Println("Sending filename and filesize!")
+	conn.Write([]byte(fileSize))
+	conn.Write([]byte(fileName))
+	sendBuffer := make([]byte, BUFFERSIZE)
+	fmt.Println("Start sending file!")
+	for {
+		_, err = file.Read(sendBuffer)
+		if err == io.EOF {
+			break
+		}
+		conn.Write(sendBuffer)
+	}
+	fmt.Println("File has been sent, closing connection!")
+	return
+}
+
+func fillString(retunString string, toLength int) string {
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
+}
